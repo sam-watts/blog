af36cefd049d753ce5ae7394f507fc1b02814882
swatts <sam.watts445@gmail.com>
2023-12-09 15:53:51 +0100

    aoc 7,8,9

--- aoc/create_quarto_doc.py
+++ aoc/create_quarto_doc.py
@@ -23,7 +23,23 @@ notes = {
     3: "I don't like grids 🫠 I probably made this harder than it needed to be. If I were to do this again I probably would have just used euclidian distance comparisons",
     4: "Enjoyed the logic for the second part with the copies. I'm sure there was potential to go on a wild goose chase with recursion here, so I'm happy to have avoided the temptation 🤣",
     5: "Stuck on part 2 for now...",
-    6: "There's definitely a more efficient way of doing part 2, but good enough :)"
+    6: "There's definitely a more efficient way of doing part 2, but good enough :)",
+    7: "Had a solution that I thought made sense but didn't give the right answer. Went to reddit for inspiration!",
+    8: "Tried to do part 2 a silly way at first - I didn't realise that all z visits happen on the same cycle for a given starting point!",
+    9: r"""
+Learned about Lagrangian Interpolation which was ...fun? In a slightly-too-small-nutshell, we
+can construct a summation of polynomials, where each term goes to zero when
+its corresponding x value is inputted to the function. Each term will be in
+the form:
+
+$$
+\frac{\text{diff}_n}{n!}\prod_{i=1}^{n}(x - i)
+$$
+
+Where $n$ is the order of the term (constant, x, x^2 etc.) and $\text{diff}_n$ is the
+difference at the correct level in the sequence of inputs. The differencing
+table 
+    """
 }
 
 
@@ -33,6 +49,7 @@ dirs = sorted(dirs, key = lambda x: int(x.name))
 for path in dirs:
     day_num = path.name
     md_file += f"## {day_num}\n"
+    md_file += f"[https://adventofcode.com/2023/day/{day_num}](https://adventofcode.com/2023/day/{day_num})\n\n"
     md_file += notes.get(int(day_num), "") + "\n"
     md_file += "\n::: {.column-page}\n"
     script_path = path / "main.py"
